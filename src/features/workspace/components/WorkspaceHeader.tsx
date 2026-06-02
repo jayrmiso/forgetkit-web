47fff4dcb12a99d7ce7becae5191984ebeff1606
Arjay Ramiso <arjayramiso@gmail.com>
2026-06-02 22:50:36 +1000

    style: shift workspace header user cluster

--- src/features/workspace/components/WorkspaceHeader.tsx
+++ src/features/workspace/components/WorkspaceHeader.tsx
@@ -135,7 +135,7 @@ export function WorkspaceHeader() {
           </div>
         </div>
 
-        <div className="flex min-w-0 items-center gap-2 justify-self-end pr-1">
+        <div className="flex min-w-0 items-center gap-2 justify-self-end -translate-x-6 lg:-translate-x-10">
           <div ref={userRef} className="relative">
             <button
               aria-expanded={userOpen}
